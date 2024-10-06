e2b9a47da657830b263c8fff2648c078b50d7193
Loga <logabaalan2004@gmail.com>
2024-10-06 14:03:03 +0530

    new commit

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -23,8 +23,8 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-
-        <Link to="app/productAdding">Product Adding</Link>
+        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/productAdding">Product Adding</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
